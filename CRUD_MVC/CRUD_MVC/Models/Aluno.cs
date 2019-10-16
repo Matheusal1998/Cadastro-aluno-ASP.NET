e7f97de40b84ea818acfa5f13030e9e9376e8635
Matheusal1998 <40475518+Matheusal1998@users.noreply.github.com>
2019-10-16 16:09:36 -0300

    Projeto base aluno

---
+++ CRUD_MVC/CRUD_MVC/Models/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_MVC.Models
+{
+    [Table("Aluno")]
+    public class Aluno
+    {
+        public int Id { get; set; }
+        [Required]
+        public String Nome { get; set; }
+        [Required]
+        [Display(Name ="Endereço")]
+        public String Endereco { get; set; }
+        [Required]
+        public String Telefone { get; set; }
+        public String Email { get; set; }
+        [Required]
+        public DateTime DataNascimento { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
